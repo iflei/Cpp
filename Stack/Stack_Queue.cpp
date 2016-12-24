05b05481367821d3662b85b4c7980de89c171bd1
iflei <admin@iflei.com>
2016-12-24 21:47:09 +0800

    栈返回最小值

--- Stack/Stack_Queue.cpp
+++ Stack/Stack_Queue.cpp
@@ -82,3 +82,52 @@ private:
 	queue<T> q2;
 };
 //实现一个栈Stack，返回最小值的操作的时间复杂度为O(1) 
+#include <iostream>
+#include <stack>
+using namespace std;
+
+template <typename T>
+class myStack
+{
+public:
+	void Push(const T& x)
+	{
+		//s.empty()说明整个栈为空
+		if(s.empty() || x <= m.top())
+		{
+			s.push(x);
+			m.push(x);
+		}
+		else
+		  s.push(x);
+	}
+
+	void Pop()
+	{
+		if(!s.empty())
+		{
+			if(s.top() == m.top())
+			{
+				s.pop();
+				m.pop();
+			}
+			else
+			  s.pop();
+		}
+	}
+
+	T Min()
+	{
+		if(!m.empty())
+		  return m.top();
+	}
+private:
+	stack<T> s;//存数据
+	stack<T> m;//存当前最小元素
+};
+
+
+
+
+
+
