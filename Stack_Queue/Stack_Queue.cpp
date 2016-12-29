117130e539a5d25c1c706c58e2fbc5afc6471f4a
iflei <admin@iflei.com>
2016-12-29 21:56:29 +0800

    元素出栈顺序的合法性

--- Stack_Queue/Stack_Queue.cpp
+++ Stack_Queue/Stack_Queue.cpp
@@ -126,8 +126,37 @@ private:
 	stack<T> m;//存当前最小元素
 };
 
+//元素出栈、入栈顺序的合法性
+#include <stack>
+#include <vector>
+using namespace std;
 
+bool IsPopOrder(vector<int> pushOrder, vector<int> popOrder)
+{
+	if(pushOrder.size() != popOrder.size() || pushOrder.size() == 0)
+	  return false;
 
+	int len = pushOrder.size();
+	int indexPopOrder = 0; //pop序列不用入栈
+	stack<int> s;
 
+	for(int i = 0; i < len && indexPopOrder < len; ++i)
+	{
+		//模拟入栈顺序
+		s.push(pushOrder[i]);
+		//如果栈顶元素等于出栈顺序中的元素，pop出栈顶元素并偏移出栈序列的下标
+		while(!s.empty() && s.top() == popOrder[indexPopOrder])
+		{
+			s.pop();
+			indexPopOrder++;
+		}
 
+		//上面循环结束有两种情况：
+		//1. i == len 说明有元素不符合出栈顺序，没有执行内层循环而++i
+		//2. indexPopOrder == len 或 s.empty() 说明出栈顺序已经遍历完了，那么栈也对应的空了
+		if(s.empty() // 等价if(indexPopOrder == len)
+			return true;
 
+		return false;
+	}
+}
