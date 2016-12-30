9a944d76a7942c5c2392defb39d212a2ff4b0f79
iflei <admin@iflei.com>
2016-12-30 08:21:06 +0800

    数组实现两个栈

--- Stack_Queue/Stack_Queue.cpp
+++ Stack_Queue/Stack_Queue.cpp
@@ -159,3 +159,74 @@ bool IsPopOrder(vector<int> pushOrder, vector<int> popOrder)
 
 	return false;
 }
+
+//用一个数组实现两个栈
+template <typename T>
+class TwoStack
+{
+public:
+	TwoStack()
+		:arr(NULL)
+		,top1(0)
+		,top2(9)
+		,capacity(10)
+	{
+		arr = new T[capacity];
+	}
+
+	~TwoStack()
+	{
+		if(arr)
+		  delete[] arr;
+	}
+
+	void Push1(const T& x)
+	{
+		_CheckCapacity();
+		arr[top1++] = x;
+	}
+
+	void Push2(const T& x)
+	{
+		_CheckCapacity();
+		arr[top2--] = x;
+	}
+
+	void Pop1()
+	{
+		if(top1 > 0)
+		  --top1;
+	}
+
+	void Pop2()
+	{
+		if(top2 < capacity - 1)
+		  ++top2;
+	}
+private:
+	void _CheckCapacity()
+	{
+		if(top1 > top2)
+		{
+			//开新空间
+			size_t oldCapacity = capacity;
+			capacity = capacity * 2; //扩容为2倍
+			T* tmp = new T[capacity];
+
+			//拷贝数据
+			for(int i = 0; i < top1; ++i)
+			  tmp[i] = arr[i];
+			for(i = oldCapacity - 1, j = capacity - 1; i > top2; --i, --j)
+			  tmp[j] = tmp[i];
+
+			delete[] arr;
+			arr = tmp;
+			top2 += oldCapacity; //调整top2
+		}
+	}
+private:
+	T* arr;
+	size_t top1;
+	size_t top2;
+	size_t capacity;
+};
