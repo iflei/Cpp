9b0832744b5af6a1b4fec44f0d40e5cf6a0c7dbb
iflei <admin@iflei.com>
2017-01-26 22:19:07 +0800

    删除倒数第k个节点O(N)

--- Linked_list/LinkList.h
+++ Linked_list/LinkList.h
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-typedef struct list_node
+typedef struct ListNode
 {
 	int data;
-	struct list_node* next;
-}node_t, *node_p, *list_p, **node_pp;
+	struct ListNode* next;
+}ListNode, *pNode, *pList, **ppList;
 
-node_pp list = NULL; //头指针
 
 //合并有序链表
-list_p merge(list_p l1, list_p l2)
+pList Merge(pList l1, pList l2)
 {
-	list_p new_head = NULL;
+	pList newHead = NULL;
 	//处理特殊情况
 	if(l1 == l2)
 	  return l1;
@@ -23,61 +22,108 @@ list_p merge(list_p l1, list_p l2)
 	  return l1;
 	//确定链表的头
 	if(l1->data < l2->data){
-		new_head = l1;
+		newHead = l1;
 		l1 = l1->next;
 	}else{
-		new_head = l2;
+		newHead = l2;
 		l2 = l2->next;
 	}
 	//连接节点
-	node_p cur = new_head;
+	pNode tail = newHead;
 	while(l1 && l2)
 	{
 		if(l1->data < l2->data){
-			cur->next = l1;
+			tail->next = l1;
 			l1 = l1->next;
 		}else{
-			cur->next = l2;
+			tail->next = l2;
 			l2 = l2->next;
 		}
-		cur = cur->next;
+		tail = tail->next;
 	}
 	//连接剩余节点
 	if(l1 != NULL){
-		cur->next = l1;
+		tail->next = l1;
 	}else{
-		cur->next = l2;
+		tail->next = l2;
 	}
 
-	return new_head;
+	return newHead;
 }
 
 //合并有序链表(递归)
-list_p merge_r(list_p l1, list_p l2)
+pList MergeR(pList l1, pList l2)
 {
-	list_p new_head = NULL;
+	pList newHead = NULL;
 	//处理特殊情况
 	if(l1 == l2)
 	  return l1;
 
-	if((l1 == NULL) && (l2 != NULL))
+	if(l1 == NULL)
 	  return l2;
 
-	if((l1 != NULL && (l2 == NULL))
+	if(l2 == NULL)
 	  return l1;
+	
 	//递归处理
 	if(l1->data < l2->data){
-		new_head = l1;
-		new_head->next = merge_r(l1->next, l2);
+		newHead = l1;
+		newHead->next = MergeR(l1->next, l2);
 	}else{
-		new_head = l2;
-		new_head->next = merge_r(l1, l2->next);
+		newHead = l2;
+		newHead->next = MergeR(l1, l2->next);
 	}
 
-	reuturn new_head;
+	reuturn newHead;
 }
 
-int main()
+//逆置单链表
+void ReverseList(ppList ppHead)
 {
-	return 0;
+	assert(ppHead); //头指针不能为空
+	pNode newHead = NULL;
+	pNode cur = *ppHead;
+	pNode prev = NULL;
+	//没有节点或只有一个节点
+	if(cur == NULL | cur->next == NULL)
+		return;
+	//调整节点next的指向
+	while(cur)
+	{
+		prev = cur;
+		cur = cur->next;
+		prev->next = newHead; //最开始newHead为NULL
+		newHead = prev;
+	}
+
+	*ppHead = newHead;
 }
+
+//删除单链表的倒数第k个节点,时间复杂度为O(N)
+void DelKNode(ppList ppHead, int k)
+{
+	assert(k > 1);
+	pNode first = *ppHead; //先走
+	pNode after = *ppHead; //后走
+	//如果k大于链表长度，first->next跳出循环时k>0
+	while(--k && first->next) //让快指针先走k-1步
+	  first = first->next;
+
+	//快慢指针一起走，当快指针走到末尾节点，慢指针走到倒数第k个节点
+	while(first->next){
+		first = first->next;
+		after = last->next;
+	}
+
+	if(k > 0){
+		return;
+	}
+	else{ //偷梁换柱法删除节点
+		pNode del = after->next;
+		after->data = del->data;
+		aftet->next = del->next;
+		free(del);
+		del = NULL;
+	}
+}
+
